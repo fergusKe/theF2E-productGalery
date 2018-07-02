fce709f44063b0a2bdfb30cb7369781ed3c16bca
fergusKe <fergus.jingtai@gmail.com>
2018-07-02 09:54:45 +0800

    add layout

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,8 +3,119 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div id="app">
-        Home
+      <div className="wrap">
+        <header>
+          <h1>
+            <small>CCCLOTHES</small>
+            MEN’S TOPS
+          </h1>
+        </header>
+        <main>
+          <div className="item item1">
+            <div className="item-content">
+              <h2 data-title="LINEN BLAZER">LINEN BLAZER</h2>
+              <p className="txt">Men’s outfit</p>
+              <p className="number">01</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1513827379601-9dba5099a3f9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjF9&s=9b19616e232bd3897c5033df23102c6e&auto=format&fit=crop&w=634&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item2">
+            <div className="item-content">
+              <h2 data-title="FREELIFT">FREELIFT</h2>
+              <p className="txt">Men’s basics</p>
+              <p className="number">02</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1514632542677-48fae74a01b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cf412aceb1d689765e387d30fca7210a&auto=format&fit=crop&w=634&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item3">
+            <div className="item-content">
+              <h2 data-title="SUPER SKINNY">SUPER SKINNY</h2>
+              <p className="txt">Men’s outfit</p>
+              <p className="number">03</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0f994eef47e5fb1a67849703cc961b3&auto=format&fit=crop&w=1050&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item4">
+            <div className="item-content">
+              <h2 data-title="DENIM">DENIM</h2>
+              <p className="txt">Men’s cadual</p>
+              <p className="number">04</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1484516758160-69878111a911?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=39466d66952b133d5a5cff642b970d05&auto=format&fit=crop&w=687&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item5">
+            <div className="item-content">
+              <h2 data-title="SWEATSHIRTS">SWEATSHIRTS</h2>
+              <p className="txt">Men’s jacket</p>
+              <p className="number">05</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1525448198276-222de3961638?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=43fb25556cdbd191e520c7f28739e40d&auto=format&fit=crop&w=1050&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item6">
+            <div className="item-content">
+              <h2 data-title="VINTAGE DENIM">VINTAGE DENIM</h2>
+              <p className="txt">Men’s classic</p>
+              <p className="number">06</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1519406709381-c1f293304b28?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=98c497a29f229085d9574ee8b6eeef8c&auto=format&fit=crop&w=1868&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+
+          <div className="item item7">
+            <div className="item-content">
+              <h2 data-title="EDITION">EDITION</h2>
+              <p className="txt">Men’s shirts</p>
+              <p className="number">07</p>
+              <div className="pic">
+                <img src="https://images.unsplash.com/photo-1516914943479-89db7d9ae7f2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=63ff45d79414b3166b08b800a32018be&auto=format&fit=crop&w=634&q=80" alt="" />
+              </div>
+              <div className="striped" />
+              <div className="bordered" />
+              <div className="color-box" />
+            </div>
+          </div>
+
+        </main>
       </div>
     );
   }
